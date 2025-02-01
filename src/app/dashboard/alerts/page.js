b0931579aebec08aad85a5fb37cfa32bb9a2e1e4
Aryanpatel335 <aryanp862002@gmail.com>
2025-02-01 18:00:04 -0500

    Alerts Page finished

--- src/app/dashboard/alerts/page.js
+++ src/app/dashboard/alerts/page.js
@@ -1,9 +1,15 @@
-import { AlertsList } from "@/components/alerts-list";
+import AlertsList from "@/components/alerts/AlertsList";
 
 export default function AlertsPage() {
   return (
     <div>
       <div className="w-full space-y-4">
+        <div className="flex items-center justify-between">
+          <h2 className="text-3xl font-bold tracking-tight">
+            Farm Alerts & Notifications
+          </h2>
+        </div>
+
         <AlertsList />
       </div>
     </div>

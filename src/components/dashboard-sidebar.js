ce35a7311c64ff900cd7ba37175f853826c4585e
martinivnv <martinivnv2002@gmail.com>
2025-02-01 12:41:33 -0500

    Add logo

--- src/components/dashboard-sidebar.js
+++ src/components/dashboard-sidebar.js
@@ -15,6 +15,7 @@ import {
 	SidebarMenuItem,
 	SidebarMenuButton,
 } from "@/components/ui/sidebar";
+import Image from "next/image";
 
 const menuItems = [
 	{
@@ -48,13 +49,22 @@ export function DashboardSidebar() {
 	return (
 		<Sidebar>
 			<SidebarHeader className="border-b p-4">
-				<h2 className="text-lg font-semibold">AgriHealth</h2>
+				<div className="flex items-center gap-4">
+					<Image
+						src="/logo.png"
+						alt="Logo"
+						width={40}
+						height={40}
+						className="rounded-full border border-gray-300"
+					/>
+					<h2 className="text-lg font-semibold text-[#2e402b]">FarmerPal</h2>{" "}
+				</div>
 			</SidebarHeader>
 			<SidebarContent>
 				<SidebarMenu>
 					{menuItems.map((item) => (
 						<SidebarMenuItem key={item.href}>
-							<SidebarMenuButton asChild>
+							<SidebarMenuButton asChild className="text-md py-6 pl-4">
 								<a href={item.href} className="flex items-center gap-2">
 									<item.icon className="h-4 w-4" />
 									<span>{item.title}</span>
